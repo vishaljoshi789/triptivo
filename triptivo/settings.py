0b2742ec48eda7053b14dc0aa6947c6c65dd0183
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-09-02 16:55:12 +0530

    updated compose file

--- triptivo/settings.py
+++ triptivo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wkh=kl2rmh8=bbaeph3dx&fm&-vm$qdln4y5nnvo)x07l3)251'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
